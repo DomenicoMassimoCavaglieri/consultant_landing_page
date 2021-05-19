63e11bac3be2bf7c8bab8095a91fca60d57435ec
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-05-19 17:33:00 +0200

    [FIXED] Mobile menu button closed
    The mobile menu button closed when pressed disappears.

--- src/assets/scripts/mobile_menu.js
+++ src/assets/scripts/mobile_menu.js
@@ -1,6 +1,14 @@
-getMenuBtnClosed().onclick = () => getMobileMenu().classList.remove("hidden");
+getMenuBtnClosed().onclick = () => {
+    getMobileMenu().classList.remove("hidden");
+    document.body.classList.add("mobile-menu-noscroll");
+    getMenuBtnClosed().classList.add("hidden");
+}
 
-getMenuBtnOpen().onclick = () => getMobileMenu().classList.add("hidden");
+getMenuBtnOpen().onclick = () => {
+    getMobileMenu().classList.add("hidden");
+    document.body.classList.remove("mobile-menu-noscroll");
+    getMenuBtnClosed().classList.remove("hidden");
+}
 
 function getMobileMenu() {
     return document.querySelector("#mobile-menu-overlay");
