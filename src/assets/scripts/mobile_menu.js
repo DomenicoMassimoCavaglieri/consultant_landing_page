cb458ac153cb034fce48e710181588ee18ab3b97
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-05-20 09:22:25 +0200

    [FIXED] Transition mobile menu
    The inbound transition of the floating menu is slower than the inbound
    transition of the overlay.

--- src/assets/scripts/mobile_menu.js
+++ src/assets/scripts/mobile_menu.js
@@ -1,16 +1,22 @@
 getMenuBtnClosed().onclick = () => {
+    getMobileMenuOverlay().classList.remove("hidden");
     getMobileMenu().classList.remove("hidden");
     document.body.classList.add("mobile-menu-noscroll");
     getMenuBtnClosed().classList.add("hidden");
 }
 
 getMenuBtnOpen().onclick = () => {
+    getMobileMenuOverlay().classList.add("hidden");
     getMobileMenu().classList.add("hidden");
     document.body.classList.remove("mobile-menu-noscroll");
     getMenuBtnClosed().classList.remove("hidden");
 }
 
 function getMobileMenu() {
+    return document.querySelector("#mobile-menu");
+}
+
+function getMobileMenuOverlay() {
     return document.querySelector("#mobile-menu-overlay");
 }
 
