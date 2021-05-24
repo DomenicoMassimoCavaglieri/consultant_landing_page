3c4bf4c1a41d13b841fd64933d2c352a1079d9de
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-05-24 15:04:45 +0200

    [ADDED] Lorem Generator Test #13
    The test is performed on a stub, which does not generate the number of
    words randomly

---
+++ src/assets/scripts/__test__/lorem.spec.js
@@ -0,0 +1,13 @@
+import {loremGenerator} from "../lorem";
+
+
+describe("Lorem Generator", () => {
+  test("it should return random string", () => {
+    const input = "Lorem, ipsum dolor sit amet";
+
+    const output = "Lorem, ipsum dolor sit";
+
+    expect(loremGenerator(input,4)).toEqual(output);
+
+  });
+});
