50d7e08360dd008f7e80d458752cc06f9fea1b60
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-05-24 16:26:01 +0200

    [ADDED] Lorem ipsum generator #15
    Lorem ipsum always starts from the first word, it can be a maximum of 66
    words and always adds a period to the last. You can choose a range of
    the number of words that will be generated randomly .

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./assets/style/main.scss";
 import "./assets/scripts/mobile_menu"; 
 import "./assets/scripts/video_player";
 import "./assets/scripts/questions";
+import "./assets/scripts/lorem";
