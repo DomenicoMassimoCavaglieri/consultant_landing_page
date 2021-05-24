b45fdb4a242424bba3dd8f7a190e0de3f6c0f868
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-05-24 17:51:45 +0200

    [ADDED] Dynamically loaded questions #16
    In the Questions section, the LOAD MORE button automatically loads
    questions and answers until the appropriate container is filled.
    When the question boxes are full, they are alternately emptied and
    filled with new questions at the press of the button, endlessly.

--- src/assets/scripts/questions.js
+++ src/assets/scripts/questions.js
@@ -1,32 +1,120 @@
-class QuestionsAnswers {
-    constructor(q, a) {
-        this.Question = q;
-        this.Answer = a;
+import { randomLoremGenMax66Words } from "./lorem";
+let numberOfQuestion = 1;
+let phaseAppendQuestions = 1;
+
+
+class QuestionAndAnswer {
+    constructor(quest, ans, num) {
+        this.question = quest;
+        this.answer = ans;
+        this.id = num;
     };
-    printQA() {
+    writeSingleQuestion() {
+        this.question = `Question ${this.id} : ${randomLoremGenMax66Words(7, 20)}`;
+        this.answer = `Answer ${this.id} : ${randomLoremGenMax66Words(3, 66)}`;
     }
 }
-let singleQuestion = new QuestionsAnswers("Come ti chiami?", "Giovanni");
-
-singleQuestion.printQA();
+let singleQuestion = new QuestionAndAnswer();
 
 
+getLoadQuestionsBtn().onclick = () => {
+    singleQuestion.id = numberOfQuestion;
+    singleQuestion.writeSingleQuestion(randomLoremGenMax66Words(7, 20),
+        randomLoremGenMax66Words(3, 66));
+    showQuestion(
+        singleQuestion.question,
+        singleQuestion.answer,
+        phaseAppendQuestions
+    )
+    //Global change
+    numberOfQuestion++;
+}
 
-getLoadQAndABtn().onclick = showQuestion;
 
-function showQuestion() {
-    if (getQuestionsBox1().childElementCount <= 7) {
-        getQuestionsBox1().appendChild(createQuestionBlock("Domanda 1", "Risposta 1: Sì, almeno un po’, qualche medico ci ha pensato sin dall’inizio, altri ci stanno arrivando adesso, è sempre più chiaro che la cura contro il Covid-19 comincia a casa, fin dai primi sintomi, quando il virus si sta già replicando nel nostro organismo senza che noi ce ne accorgiamo. I primi dieci giorni sono cruciali per fermare il virus, prima che raggiunga le cellule del sangue per poi diffondersi dappertutto, quei dieci giorni non li dobbiamo perdere. E di tutte le cose che si possono fare a casa, la prima e la più importante è incontrare il proprio medico. «How are you in yourself?», chiedeva sempre Stuart Cameron — grande nefrologo e grandissimo medico — ai suoi pazienti. Sarebbe «Come ti senti dentro te stesso?»: a un medico bravo basta questo per capire come vanno le cose. Poi certo, ci sono anche i farmaci, anche se sulle cure a casa gli studi controllati (quelli che hanno valore scientifico) sono davvero pochini; ecco perché è sbagliato criticare le indicazioni del Ministero e dell’Aifa nella gestione domiciliare dei pazienti con infezione da SARS-CoV-2, senza studi impeccabili e pubblicati, le istituzioni non possono fare di più"));
-    } else if (getQuestionsBox2().childElementCount <= 6) {
-        getQuestionsBox2().appendChild(createQuestionBlock("Domanda 1", "Risposta 1: Sì, almeno un po’, qualche medico ci ha pensato sin dall’inizio, altri ci stanno arrivando adesso, è sempre più chiaro che la cura contro il Covid-19 comincia a casa, fin dai primi sintomi, quando il virus si sta già replicando nel nostro organismo senza che noi ce ne accorgiamo. I primi dieci giorni sono cruciali per fermare il virus, prima che raggiunga le cellule del sangue per poi diffondersi dappertutto, quei dieci giorni non li dobbiamo perdere. E di tutte le cose che si possono fare a casa, la prima e la più importante è incontrare il proprio medico. «How are you in yourself?», chiedeva sempre Stuart Cameron — grande nefrologo e grandissimo medico — ai suoi pazienti. Sarebbe «Come ti senti dentro te stesso?»: a un medico bravo basta questo per capire come vanno le cose. Poi certo, ci sono anche i farmaci, anche se sulle cure a casa gli studi controllati (quelli che hanno valore scientifico) sono davvero pochini; ecco perché è sbagliato criticare le indicazioni del Ministero e dell’Aifa nella gestione domiciliare dei pazienti con infezione da SARS-CoV-2, senza studi impeccabili e pubblicati, le istituzioni non possono fare di più"));
+
+
+
+/*These functions handle the addition of the Question Block component
+This section of the DOM is divided into two boxes*/
+/**
+ * This manages the addition of questions by dividing it into 6 phases
+ * @param {string} question Question text
+ * @param {string} answer Answer text
+ * @param {integer} counter Counter of the phases 
+ */
+function showQuestion(question, answer, counter) {
+    switch (counter) {
+        case 1:
+            appendQuestionsOnBox1(question, answer, 2);
+            break;
+        case 2:
+            appendQuestionsOnBox2(question, answer, 3);
+            break;
+        case 3:
+            resetQuestionOnBox1(question, answer, 4);
+            break;
+        case 4:
+            appendQuestionsOnBox1(question, answer, 5);
+            break;
+        case 5:
+            resetQuestionOnBox2(question, answer, 6);
+            break;
+        case 6:
+            appendQuestionsOnBox2(question, answer, 3);
+            break;
+    }
+
+}
+
+//This removes all questions from box-2 and adds a new one
+function resetQuestionOnBox2(question, answer, nextCase) {
+    while (getQuestionsBox2().childElementCount > 1) {
+        getQuestionsBox2().removeChild(getQuestionsBox2().lastElementChild);
+    }
+    getQuestionsBox2().appendChild(createQuestionBlock(question, answer));
+    phaseAppendQuestions = nextCase;
+}
+
+//This removes all questions from box-1 and adds a new one
+function resetQuestionOnBox1(question, answer, nextCase) {
+    while (getQuestionsBox1().childElementCount > 1) {
+        getQuestionsBox1().removeChild(getQuestionsBox1().lastElementChild);
+    }
+    getQuestionsBox1().appendChild(createQuestionBlock(question, answer));
+    phaseAppendQuestions = nextCase;
+}
+
+//This adds the questions in the box-2
+function appendQuestionsOnBox2(question, answer, nextCase) {
+    if (getQuestionsBox2().childElementCount <= 5) {
+        getQuestionsBox2().appendChild(createQuestionBlock(question, answer));
+    } else if (getQuestionsBox2().childElementCount === 6) {
+        getQuestionsBox2().appendChild(createQuestionBlock(question, answer));
+        //Global change
+        phaseAppendQuestions = nextCase;
     }
 }
 
+//This adds the questions in the box-1
+function appendQuestionsOnBox1(question, answer, nextCase) {
+    if (getQuestionsBox1().childElementCount <= 6) {
+        getQuestionsBox1().appendChild(createQuestionBlock(question, answer));
+    } else if (getQuestionsBox1().childElementCount === 7) {
+        getQuestionsBox1().appendChild(createQuestionBlock(question, answer));
+        //Global change
+        phaseAppendQuestions = nextCase;
+    }
+}
+
+
+//These functions manage generation and functionality of the Question Block component:
+
+//This function manages the visibility of the answer in the Question Block component
 function answertoggle(event) {
     let arrow = event.target;
     let answer = event.target.parentNode.nextElementSibling;
     let answerHeight = answer.scrollHeight;
-    
+
     let visibleAnswers = getQuestionsWrapper().querySelectorAll(".visible-answer");
     let rotateArrows = getQuestionsWrapper().querySelectorAll(".rotate-arrow")
 
@@ -49,10 +137,15 @@ function answertoggle(event) {
     for (let rotateArrow of rotateArrows) {
         rotateArrow.classList.remove("rotate-arrow");
     }
-
-    console.log(visibleAnswers);
 }
 
+/**
+ * This function generates the Question Block component 
+ * and accepts text for Q&A as arguments
+ * @param {string} Question 
+ * @param {string} Answer 
+ * @returns {Html Element} Question component
+ */
 function createQuestionBlock(question, answer) {
     let singleQuestion = document.createElement("div");
     singleQuestion.classList.add("single-question");
@@ -73,11 +166,13 @@ function createQuestionBlock(question, answer) {
     questionLabel.appendChild(answerArrow);
     singleQuestion.appendChild(questionLabel);
     singleQuestion.appendChild(answerText);
-
     return singleQuestion;
 }
 
-function getLoadQAndABtn() {
+
+//These functions return elements within the Questions Section
+
+function getLoadQuestionsBtn() {
     return document.querySelector("#loadQuestionsBtn");
 }
 
