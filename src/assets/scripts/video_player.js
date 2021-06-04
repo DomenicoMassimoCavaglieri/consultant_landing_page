340eb6971210f2a3efd4d5598577febf583eca0b
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-06-04 05:45:45 +0200

    [FIXED] Video player bug #29
    A CSS transition was preventing the STOP button from functioning
    correctly on touch devices. The general functionality has also been
    improved: a specific function has been added to display the controls and
    make them disappear automatically, linked to the "ontouchstart". The
    management of the HTML tags that make up the player has also been
    improved.

--- src/assets/scripts/video_player.js
+++ src/assets/scripts/video_player.js
@@ -1,104 +1,131 @@
+//The video player is dynamically created. 
+//The play button becomes stop if the video is running and vice versa. 
+//The controls are disabled if the video is stopped and appear if the video is playing 
+//and if there is user interaction.
 
 import Video from "../video/company_video.mp4"
 
+//Video status control variable (Play / Stop)
 let videoOn = false;
+
 
 initVideoSection();
 
+
+//Loading the initial settings
 function initVideoSection() {
     createVideoOverlay();
     createVideo();
+
     createIconPlayStop();
     setVideo();
 
+    getVideo().ontouchstart = handleTouch;
     getIconPlayStop().onmousemove = showVideoControls;
-    getIconPlayStop().onmouseleave = HideVideoControls;
+    getIconPlayStop().onmouseout = HideVideoControls;
     getVideo().onmousemove = showVideoControls;
-    getVideo().onmouseleave = HideVideoControls;
+    getVideo().onmouseout = HideVideoControls;
     getVideo().onended = setVideoStop;
     getIconPlayStop().onclick = playOrStop;
 
 }
 
+
+//A single button manages Play and Stop
 function playOrStop() {
     (videoOn) ?  setVideoStop() : setVideoPlay();
     
 }
 
+//The stop key appears / disappears on the touch together with the native controls
+function handleTouch() {
+    setTimeout( (showVideoControls), 400);
+    setTimeout( (HideVideoControls), 3000);
+}
+
+//Hides stop key and native controls
 function HideVideoControls() {
     if (videoOn) {
-        getIconPlayStop().classList.add("hidden");
+        getIconPlayStop().classList.add("hidden-notransition");
     }
 }
 
+//Shows stop key and native controls
 function showVideoControls() {
     if (videoOn) {
         getVideo().controls = true;
         getIconPlayStop().classList.add("icon-stop");
-        getIconPlayStop().classList.remove("hidden");
+        getIconPlayStop().classList.remove("hidden-notransition");
     }
 }
 
+//This stops the video, shows the poster, turns the stop button to play and moves it to the center
 function setVideoStop() {
-    getIconPlayStop().classList.add("hidden");
-    setTimeout( () => getIconPlayStop().classList.remove("icon-stop"),400);
-    setTimeout( () => getVideo().classList.add("hidden"),600);
-    setTimeout( () => getVideoOverlay().classList.add("video-overlay-poster"),100);
-    
+    getVideo().load()
+    setTimeout( () => getVideoOverlay().classList.add("video-overlay-poster"),500);
+    getIconPlayStop().classList.add("hidden-notransition");
+    setTimeout( () => getIconPlayStop().classList.remove("icon-stop"),450);
     getIconPlayStop().classList.add("icon-play");
-    setTimeout( () => getIconPlayStop().classList.remove("hidden"),1200);
+    setTimeout( () => getIconPlayStop().classList.remove("hidden-notransition"),800);
     getVideo().controls = false;
-    setTimeout( () => getVideo().load(),900);
     videoOn = false;
 }
 
+//This starts the video, hides the poster, turns the play button to stop and moves it to the side
 function setVideoPlay() {
     getVideoOverlay().classList.remove("video-overlay-poster");
-    getIconPlayStop().classList.add("hidden");
-    getVideo().classList.remove("hidden");
+    //getIconPlayStop().classList.remove("icon-play");
+    getIconPlayStop().classList.add("icon-stop");
+    getIconPlayStop().classList.add("hidden-notransition");
     getVideo().play();
     videoOn = true;
 }
 
+//This assigns the video file and sets the volume to half
 function setVideo() {
     getVideo().src = Video;
     getVideo().volume = 0.5;
 }
 
-function getIconPlayStop() {
-    return document.querySelector("#icon-video");
+//Creation of the Play / Stop button
+function createIconPlayStop() {
+    const iconPlay = document.createElement("div");
+    iconPlay.classList.add("icon-play");
+    iconPlay.setAttribute("id", "icon-video");
+    getVideoWrapper().appendChild(iconPlay);
 }
 
-function getVideo() {
-    return document.querySelector("#video");
+//Creating the overlay poster
+function createVideoOverlay() {
+    const videoOverlay = document.createElement("div");
+    videoOverlay.setAttribute("id", "video-overlay");
+    videoOverlay.classList.add("video-overlay-poster");
+    getVideoWrapper().appendChild(videoOverlay);
+}
+
+//Creating the video tag and assigning the corresponding file
+function createVideo() {
+    const video = document.createElement("video");
+    video.setAttribute("id", "video");
+    getVideoWrapper().appendChild(video);
+
+}
+
+
+//These return elements of the DOM that make up the video section
+
+function getIconPlayStop() {
+    return document.querySelector("#icon-video");
 }
 
 function getVideoOverlay() {
     return document.querySelector("#video-overlay");
 }
 
+function getVideo() {
+    return document.querySelector("#video");
+}
+
 function getVideoWrapper() {
     return document.querySelector("#video-wrapper");
-}
-
-function createIconPlayStop() {
-    const iconPlay = document.createElement("div");
-    iconPlay.classList.add("icon-play");
-    iconPlay.setAttribute("id", "icon-video");
-    getVideoOverlay().appendChild(iconPlay);
-}
-
-function createVideo() {
-    const video = document.createElement("video");
-    video.setAttribute("id", "video");
-    video.classList.add("hidden");
-    getVideoOverlay().appendChild(video);
-
-}
-
-function createVideoOverlay() {
-    const videoOverlay = document.createElement("div");
-    videoOverlay.setAttribute("id", "video-overlay");
-    videoOverlay.classList.add("video-overlay-poster");
-    getVideoWrapper().appendChild(videoOverlay);
 }
