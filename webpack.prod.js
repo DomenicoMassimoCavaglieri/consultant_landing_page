9c0f3d20ffd1b372f94dee7ef1602d0100221b22
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-06-05 15:52:46 +0200

    [FIXED] Webpack configuration
    Removed some comments and restored the functionality of
    HtmlWebpackPlugin in the dev config.

--- webpack.prod.js
+++ webpack.prod.js
@@ -19,21 +19,6 @@ module.exports = merge(common, {
             title: 'Custom template',
             template: "./src/template.html",
             inject: 'body',
-
-            // 'meta': {
-            //     'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
-            //     //<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-            //     'Content-Security-Policy': { 'http-equiv': 'Content-Security-Policy', 'content': 'default-src https:' },
-            //     //<meta http-equiv="Content-Security-Policy" content="default-src https:">
-
-            //     'og:image': { 'property': 'og:image', 'content': '../src/assets/images/open_graph_image.jpg' },
-            //     //<meta property="og:image" content="../src/assets/images/open_graph_image.jpg">
-
-            //     'twitter:image': '../src/assets/images/open_graph_image.jpg'
-            //     //<meta name="twitter:image" content="../src/assets/images/open_graph_image.jpg">
-            // }
-
         }),
         new MiniCssExtractPlugin({
             filename: "[name].css",
